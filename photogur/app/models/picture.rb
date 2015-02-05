0f7ee342e69a677940c17e5c6b7d59797f1b0ac0
jmy19 <jasonmyers@rogers.com>
2015-02-05 16:46:51 -0500

    Display most recent pictures on index

--- photogur/app/models/picture.rb
+++ photogur/app/models/picture.rb
@@ -1,2 +1,9 @@
 class Picture < ActiveRecord::Base
+	scope :newest_first, -> { order(“created_at DESC”) }
+  scope :most_recent_five, -> { newest_first.limit(5) }
+  
+  def self.created_before(time)
+    where("created_at < ?", time) 
+  end
+  
 end
